0e69439f7dbe34fb4c478c27f3d304321316ec25
attabooi <joonqpa@gmail.com>
2022-06-22 12:04:15 +0900

    ADD: Article Serializers, Admin 페이지 추가

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from article.models import Article, Comment, Like, Categories
 
 # Register your models here.
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Categories)
--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,35 @@
 from rest_framework import serializers
 from user.models import User, UserProfile
+from article.models import Article, Comment, Like, Categories
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    articles_user = serializers.SerializerMethodField()
+
+    def get_articles_user(self,obj):
+        return obj.user.username
+
+    class Meta:
+        model = Article
+        fields = ["title", "body", "articles_user"]
+
+class CommentSerializer(serializers.ModelSerializer):
+    comments_user = serializers.SerializerMethodField()
+
+    def get_comments_user(self,obj):
+        return obj.user.username
+    class Meta:
+        model = Comment
+        fields = ["comments", "comments_user"]
+
+class LikeSerializer(serializers.ModelSerializer):
+    likes_user = serializers.SerializerMethodField()
+
+    def get_likes_user(self,obj):
+        return obj.user.username
+    class Meta:
+        model = Like
+        fields = ["articles", "likes_user"]
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
@@ -11,8 +41,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     userprofile = UserProfileSerializer()
+    comment_set = CommentSerializer(many=True)
+    article_set = ArticleSerializer(many=True)
+    like_set = LikeSerializer(Many=True)
     class Meta:
         # serializer에 사용될 model, field지정
         model = User
         # 모든 필드를 사용하고 싶을 경우 fields = "__all__"로 사용
-        fields = "__all__"
+
+        fields = ['username', 'userprofile', 'article_set', 'comment_set', 'like_set']
